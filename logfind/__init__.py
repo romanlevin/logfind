44e1556f6571c8767bd2e38700969eb863d8b167
Roman Levin <romanlevin@gmail.com>
2015-06-19 13:46:19 +0200

    Use regular expressions
    Implemented using searching over `mmap`ped files

--- logfind/__init__.py
+++ logfind/__init__.py
@@ -2,6 +2,8 @@ import argparse
 import errno
 import sys
 import os
+import re
+import mmap
 
 
 LOGFIND_RC = os.path.expanduser('~/.logfind')
@@ -19,44 +21,40 @@ def get_important_files(logfind_rc=LOGFIND_RC):
         raise
 
 
-def scan_file(file_object, search_terms, and_lookup=True):
-    search_term_set = set(search_terms)
-    file_is_a_hit = False
-    for line in file_object:
-        present_term_set = {term for term in search_term_set if term in line}
-        if and_lookup:
-            search_term_set -= present_term_set
-            file_is_a_hit = not search_term_set
-        else:
-            file_is_a_hit = bool(present_term_set)
-
-        if file_is_a_hit:
-            return True
+def scan_file(file_object, patterns, and_lookup=True):
+    any_or_all = all if and_lookup else any
+    with mmap.mmap(file_object.fileno(), 0) as data:
+        return any_or_all(pattern.search(data) for pattern in patterns)
 
 
-def scan_file_path(file_path, search_terms, and_lookup=True):
+def scan_file_path(file_path, patterns, and_lookup=True):
     try:
         with open(file_path) as f:
-            return scan_file(f, search_terms, and_lookup=and_lookup)
+            return scan_file(f, patterns, and_lookup=and_lookup)
     except IOError as e:
         # Silently skip if the file does not exist
         if not e.errno == errno.ENOENT:
             raise
 
 
+def compile_patterns(patterns):
+    return tuple(re.compile(pattern) for pattern in patterns)
+
+
 def get_args():
     parser = argparse.ArgumentParser(description='Scan files for search terms')
-    parser.add_argument('words', metavar='word', type=str, nargs='+', help='search for these words in each file. Assumes AND between each word')
-    parser.add_argument('-o', '--or-lookup', help='assume OR between each word', action='store_true')
+    parser.add_argument('patterns', metavar='pattern', type=bytes, nargs='+', help='search for these regexp patterns in each file. Assumes AND between each pattern')
+    parser.add_argument('-o', '--or-lookup', help='assume OR between each pattern', action='store_true')
     args = parser.parse_args()
     return args
 
 
 def main():
     args = get_args()
-    words = args.words
+    patterns = args.patterns
     and_lookup = not args.or_lookup
     paths_to_scan = get_important_files()
-    hits = [path for path in paths_to_scan if scan_file_path(path, words, and_lookup=and_lookup)]
+    compiled_patterns = compile_patterns(patterns)
+    hits = [path for path in paths_to_scan if scan_file_path(path, compiled_patterns, and_lookup=and_lookup)]
     for hit in hits:
         print(hit)
--- tests/test_logfind.py
+++ tests/test_logfind.py
@@ -1,45 +1,58 @@
-from io import StringIO
 import logfind
+import tempfile
+from contextlib import contextmanager
+
+
+@contextmanager
+def mock_file(contents):
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(contents)
+        f.flush()
+        yield f
 
 
 def test_scan_file_positive_and():
-    mock_file_contents = """
+    mock_file_contents = b"""
 term1
 term3
 term2
 """
-    terms = ('term1', 'term2')
-    mock_file = StringIO(mock_file_contents)
-    result = logfind.scan_file(mock_file, terms)
+    terms = (b'term1', b'term2')
+    patterns = logfind.compile_patterns(terms)
+    with mock_file(mock_file_contents) as f:
+        result = logfind.scan_file(f, patterns)
     assert result
 
 
 def test_scan_file_negative_and():
-    mock_file_contents = """
+    mock_file_contents = b"""
 term1
 term3
 """
-    terms = ('term1', 'term2')
-    mock_file = StringIO(mock_file_contents)
-    result = logfind.scan_file(mock_file, terms)
+    terms = (b'term1', b'term2')
+    patterns = logfind.compile_patterns(terms)
+    with mock_file(mock_file_contents) as f:
+        result = logfind.scan_file(f, patterns)
     assert not result
 
 
 def test_scan_file_positive_or():
-    mock_file_contents = """
+    mock_file_contents = b"""
 term1
 """
-    terms = ('term1', 'term2')
-    mock_file = StringIO(mock_file_contents)
-    result = logfind.scan_file(mock_file, terms, and_lookup=False)
+    terms = (b'term1', b'term2')
+    patterns = logfind.compile_patterns(terms)
+    with mock_file(mock_file_contents) as f:
+        result = logfind.scan_file(f, patterns, and_lookup=False)
     assert result
 
 
 def test_scan_file_negative_or():
-    mock_file_contents = """
+    mock_file_contents = b"""
 term3
 """
-    terms = ('term1', 'term2')
-    mock_file = StringIO(mock_file_contents)
-    result = logfind.scan_file(mock_file, terms, and_lookup=False)
+    terms = (b'term1', b'term2')
+    patterns = logfind.compile_patterns(terms)
+    with mock_file(mock_file_contents) as f:
+        result = logfind.scan_file(f, patterns, and_lookup=False)
     assert not result
